80866397ec6855455a1f61bd84cdcc40ede0a2c2
Namratha2604 <gundanamratha@gmail.com>
2024-12-01 15:16:49 +0530

    event handling

--- app/src/App1.jsx
+++ app/src/App1.jsx
@@ -2,24 +2,29 @@ import { useState } from "react";
 
 const App1 = () => {
 
-const [name, setName] = useState("nasar");
+// const [name, setName] = useState("nasar");
+const [form, setForm] = useState({tel:"", add:""});
 
-const handleClick = ()=>{
-    alert("im cloickfc");
-}
-const handleMouse = ()=>{
-    alert("beeeeeep beeep")
-}
+// const handleClick = ()=>{
+//     alert("im cloickfc");
+// }
+// const handleMouse = ()=>{
+//     alert("beeeeeep beeep")
+// }
 
 const handleChange = (i)=>{
-    setName(i.target.value);
+    // setName(i.target.value);
+    setForm({...form,[i.target.tel]:i.target.value});
+    console.log(form);
 }
 
   return (
     <div>
-      <button onClick={handleClick} >click me</button>
-      <p onMouseOver={handleMouse} >jordindiannnn</p>
-      <input type="text" value={name} onChange={handleChange} />
+      {/* <button onClick={handleClick} >click me</button>
+      <p onMouseOver={handleMouse} >jordindiannnn</p> */}
+      <input type="text" name='tel' value={form.tel} onChange={handleChange} />
+
+      <input type="text" name='add' value={form.add} onChange={handleChange} />
     </div>
   )
 }
